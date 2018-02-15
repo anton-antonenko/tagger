5d82dd30432ab74e71df495fb8569c7ddbf90dc3
anton.antonenko <anton.antonenko@kohls.com>
2018-02-15 15:24:43 -0500

    add large data files

--- main.py
+++ main.py
@@ -21,16 +21,17 @@ def load_model(path):
 # x_valid, y_valid = load_data_and_labels('anago/data/conll2003/en/ner/valid.txt')
 # x_test, y_test = load_data_and_labels('anago/data/conll2003/en/ner/test.txt')
 
-x_train, y_train = load_data_and_labels('data/train.txt')
-x_valid, y_valid = load_data_and_labels('data/test.txt')
+x_train, y_train = load_data_and_labels('data/training.txt')
+x_valid, y_valid = load_data_and_labels('data/validation.txt')
 # x_test, y_test = load_data_and_labels('data/test.txt')
 
+test_examples_num = 100000
 model = anago.Sequence()
-model.train(x_train, y_train, x_valid, y_valid)
+model.train(x_train[:test_examples_num], y_train[:test_examples_num], x_valid[:test_examples_num], y_valid[:test_examples_num])
 
 # model.eval(x_test, y_test)
 
-# model.save('my-model/')
+model.save('big-model/')
 
 # model = load_model('model/')
 # model = load_model('my-model/')
